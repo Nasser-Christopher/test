83fe22ffe9798d456f06a018e40b3486be9cdb82
Nasser-Christopher <christopher.nasser@gmail.com>
2022-12-27 13:04:37 -0800

    Updated plan to solve in comments

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -8,10 +8,9 @@ var productExceptSelf = function(nums) {
     for (let index = 0; index < nums.length; index++)
     {
         // TODO: Two For loops (not nested), one to iterate, the other to multiply
+        // --:In first loop, check for any 0's; if found, zeroesFound++
         // TODO: Check for edge cases (e.g. if val @ curr index = 0)
-
-
-        
+        // --:If edge case found, make all other results 0 (0*x=0)
          
     }
     return res;
