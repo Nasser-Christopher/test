5ef175facb7a1b65569c2e251cf060a767c0bb73
Nasser-Christopher <christopher.nasser@gmail.com>
2022-12-27 13:45:37 -0800

    Fixed structure error from previous commit

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -4,21 +4,28 @@
  */
 var productExceptSelf = function(nums) {
     let res = Array.from(nums);
-
-    for (let index = 0; index < nums.length; index++)
-    {
+       
         // TODO: Two For loops (not nested), one to iterate, the other to multiply
         // --:In first loop, check for any 0's; if found, zeroesFound++
         // TODO: Check for edge cases (e.g. if val @ curr index = 0)
         // --:If edge case found, make all other results 0 (0*x=0)
+        // TODO: Keep count of TotalProduct and divide it out later
 
         let zeroesFound = 0;
+        let productOfArray = 1;
 
+        // Check for 0
         for (let index = 0; index < nums.length; index++)
         {
-            
+            if (nums[index] == 0) // 0 encountered in array
+                zeroesFound++;
+            else
+                productOfArray *= nums[index];
+
         }
+
+
          
-    }
+
     return res;
 };
