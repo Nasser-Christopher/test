cf989f2d601075a76c20f680a912e1d1c66b484a
Nasser-Christopher <christopher.nasser@gmail.com>
2023-01-01 19:16:12 -0800

    Made comments so I could understand the logic better.

--- climbStairs.js
+++ climbStairs.js
@@ -10,13 +10,18 @@ var climbStairs = function(n) {
     // n = 1
     if (n == 1)
         return 1;
-
+        
+    // n >= 3, execute loop logic and modify 'stepTwo' 
+    // to be returned later
     for (let i = 3; i <= n; i++)
     {
         let tempVal = stepOne + stepTwo;
         stepOne = stepTwo;
         stepTwo = tempVal;
     }
+
+    // returns default val of 2 if n = 2
+    // else, returns modified value done by loop
     return stepTwo;
     
     
