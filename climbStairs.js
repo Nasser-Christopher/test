8c390a820a3818fbee53503dc84fed1ec8ddc327
Nasser-Christopher <christopher.nasser@gmail.com>
2023-01-01 18:25:01 -0800

    Testing code/ Initial structure idea

--- climbStairs.js
+++ climbStairs.js
@@ -4,4 +4,17 @@
  */
 var climbStairs = function(n) {
     
+    let distinctMoves = [];
+
+    // n = 1
+    if (n == 0)
+        return 1;
+
+    if (distinctMoves[n] != 0)
+        return distinctMoves[n];
+
+    return distinctMoves[n] = climbStairs(n-1) + climbStairs(n-2);
+
+    
+
 };
